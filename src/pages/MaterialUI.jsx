94ec502a3b9cf94c6728506b41c40617c57077ea
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-04-09 13:07:05 +0530

    Components Adde using Material UI

--- src/pages/MaterialUI.jsx
+++ src/pages/MaterialUI.jsx
@@ -1,17 +1,27 @@
-import { Button } from '@mui/material';
+import { Button, Switch } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
-import { Style } from '@mui/icons-material';
+const label = { inputProps: { 'aria-label': 'Switch demo' } };
+import ResponsiveAppBar from '../components/ResponsiveAppBar';
+import RecipeReviewCarf from '../components/RecipeReviewCard';
 
 export default function MaterialUI() {
+
+    const handleBtn = () => {
+        console.log("Hi");
+    }
+
     return (
         <div>
             <h1>Hello Material-UI</h1>
-            <Button variant="contained">Contained</Button>
+            <Button variant="contained" onClick={handleBtn}>Contained</Button>
             <h1>
                 <DeleteIcon style={{
                     fontSize: 100,
                     color: 'red'
                 }} />
+                <Switch {...label} />
+                <ResponsiveAppBar />
+                <RecipeReviewCarf />
             </h1>
         </div>
     );
