91410fa58dde1c3d09430c9591817ddb83817932
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-04-09 11:37:23 +0530

    Bootstrap Completed

--- src/pages/Bootstrap.jsx
+++ src/pages/Bootstrap.jsx
@@ -2,6 +2,8 @@ export default function Bootstrap() {
     return (
         <div>
             <h1>Hello Bootstrap</h1>
+            <button type="button" class="btn btn-primary">Primary</button>
+            <i class="bi bi-people"></i>
         </div>
     );
 }
