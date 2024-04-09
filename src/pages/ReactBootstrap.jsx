a31734b40b3a45031952937d48a4401b6f8c9412
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-04-09 12:47:21 +0530

    Bootstrap Icons Refactored

--- src/pages/ReactBootstrap.jsx
+++ src/pages/ReactBootstrap.jsx
@@ -6,7 +6,10 @@ export default function ReactBootstrap() {
             <h1>Hello React-Bootstrap</h1>
             <Button variant="primary">Primary</Button>
             <h1>
-                <i className="bi bi-people"></i>
+                <i className="bi bi-people" style={{
+                    fontSize: 100,
+                    color: 'red'
+                }}> </i>
             </h1>
         </div>
     );
